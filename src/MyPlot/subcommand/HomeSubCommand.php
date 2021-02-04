50c08c42d6b47d7d160750a1440a1f3ac856ba51
Lyco Tatierra <lycol50@gmail.com>
2021-02-04 23:40:59 +0800

    Add files via upload

---
+++ src/MyPlot/subcommand/HomeSubCommand.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+namespace MyPlot\subcommand;
+
+use MyPlot\forms\MyPlotForm;
+use MyPlot\forms\subforms\HomeForm;
+use MyPlot\Plot;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class HomeSubCommand extends SubCommand
+{
+	/**
+	 * @param CommandSender $sender
+	 *
+	 * @return bool
+	 */
+	public function canUse(CommandSender $sender) : bool {
+		return ($sender instanceof Player) and $sender->hasPermission("myplot.command.home");
+	}
+
+	/**
+	 * @param Player $sender
+	 * @param string[] $args
+	 *
+	 * @return bool
+	 */
+	public function execute(CommandSender $sender, array $args) : bool {
+		if(empty($args)) {
+			$plotNumber = 1;
+		}elseif(is_numeric($args[0])) {
+			$plotNumber = (int) $args[0];
+		}else{
+			return false;
+		}
+		$levelName = $args[1] ?? $sender->getLevel()->getFolderName();
+		$plots = $this->getPlugin()->getPlotsOfPlayer($sender->getName(), $levelName);
+		if(empty($plots)) {
+			$sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
+			return true;
+		}
+		if(!isset($plots[$plotNumber - 1])) {
+			$sender->sendMessage(TextFormat::RED . $this->translateString("home.notexist", [$plotNumber]));
+			return true;
+		}
+		usort($plots, function(Plot $plot1, Plot $plot2) {
+			if($plot1->levelName == $plot2->levelName) {
+				return 0;
+			}
+			return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
+		});
+		/** @var Plot $plot */
+		$plot = $plots[$plotNumber - 1];
+		if($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
+			$sender->sendMessage($this->translateString("home.success", [$plot->__toString(), $plot->levelName]));
+			$sender->setGamemode(1);
+		}else{
+			$sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
+		}
+		return true;
+	}
+
+	public function getForm(?Player $player = null) : ?MyPlotForm {
+		if(count($this->getPlugin()->getPlotsOfPlayer($player->getName(), $player->getLevel()->getFolderName())) > 0)
+			return new HomeForm($player);
+		return null;
+	}
+}
